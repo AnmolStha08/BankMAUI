0cd450648db9997a3a20910bf99b784d5048ea47
AnmolStha08 <anmolstha08gmail.com>
2024-12-18 09:19:51 +0545

    Add project files.

---
+++ BankMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BankMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ BankMAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BankMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
